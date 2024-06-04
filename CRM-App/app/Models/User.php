e3d30646b756a98934a39ec91a4b7ebb01fcb8e1
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-06-04 16:05:24 +0100

    add User Model

--- CRM-App/app/Models/User.php
+++ CRM-App/app/Models/User.php
@@ -18,7 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'city',
+        'current_address',
+        'profile_picture',
         'email',
         'password',
     ];
