95958f8de43acba86e95ba0566df0c679aa3a900
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-06-07 15:01:51 +0100

    Create authSlice.js

---
+++ crm-frontend/src/store/authSlice.js
@@ -0,0 +1,42 @@
+// authSlice.js
+
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const fetchUser = createAsyncThunk('auth/fetchUser', async () => {
+    const response = await fetch('http://127.0.0.1:8000/api/authUser', {
+        credentials: 'include' 
+    });
+    if (!response.ok) {
+        throw new Error('Failed to fetch user');
+    }
+    const data = await response.json();
+    console.log(data.user);
+    return data.user; 
+});
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+        loading: false,
+        error: null
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchUser.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchUser.fulfilled, (state, action) => {
+                state.loading = false;
+                state.user = action.payload;
+            })
+            .addCase(fetchUser.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message;
+            });
+    }
+});
+
+export default authSlice.reducer;
