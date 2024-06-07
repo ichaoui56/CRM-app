586e662b20a0d911c21f71ee91d3dd5eb20d2c80
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-06-07 15:01:25 +0100

    Create useAuth.js

---
+++ crm-frontend/src/hooks/useAuth.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const useAuth = () => {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const login = async (email, password) => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            const response = await axios.post('http://127.0.0.1:8000/api/login', {
+                email: email,
+                password: password,
+            });
+
+            const { token } = response.data;
+            console.log(response.data);
+            localStorage.setItem('token', token);
+            if (localStorage.setItem('token', token)) {
+            }
+            axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+            setLoading(false);
+            return true;
+        } catch (err) {
+            setError(err.response ? err.response.data : 'Login failed');
+            setLoading(false);
+            return false;
+        }
+    };
+
+    return { login, loading, error };
+};
+
+export default useAuth;
