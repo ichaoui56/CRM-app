0ea7a9027b13b0b4c5a3fa62e3d2340c15f6ac7a
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-06-07 15:01:09 +0100

    Update header.js

--- crm-frontend/src/components/partials/dashboard/HeaderStyle/header.js
+++ crm-frontend/src/components/partials/dashboard/HeaderStyle/header.js
@@ -2,6 +2,8 @@ import React, { useEffect, Fragment, memo } from 'react'
 import { Navbar, Container, Nav, Dropdown } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import CustomToggle from '../../../dropdowns'
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchUser } from '../../../../store/authSlice.js';
 
 //img
 import flag1 from '../../../../assets/images/Flag/flag001.png'
@@ -24,15 +26,19 @@ import avatars6 from '../../../../assets/images/avatars/avtar_5.png'
 // logo
 import Logo from '../../components/logo'
 
-// Redux Selector / Action
-import { useSelector } from 'react-redux';
-
 // Import selectors & action from setting store
 import * as SettingSelector from '../../../../store/setting/selectors.ts'
 
 const Header = memo((props) => {
     const navbarHide = useSelector(SettingSelector.navbar_show); // array
     const headerNavbar = useSelector(SettingSelector.header_navbar)
+
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.auth.user);
+    
+    const userName = user ? user.first_name : '';
+    const userEmail = user ? user.email : '';
+
     useEffect(() => {
         // navbarstylemode
         if (headerNavbar === 'navs-sticky' || headerNavbar === 'nav-glass') {
@@ -44,8 +50,9 @@ const Header = memo((props) => {
                 }
             }
         }
-
-    })
+        console.log(fetchUser);
+        dispatch(fetchUser());
+    }, [dispatch]);
     const minisidebar = () => {
         document.getElementsByTagName('ASIDE')[0].classList.toggle('sidebar-mini')
     }
@@ -82,14 +89,7 @@ const Header = memo((props) => {
                     </Navbar.Toggle>
                     <Navbar.Collapse id="navbarSupportedContent">
                         <Nav as="ul" className="mb-2 ms-auto navbar-list mb-lg-0 align-items-center">
-                            <Nav.Link className="btn btn-primary btn-sm d-flex gap-2 align-items-center me-2" href="https://hopeui.iqonic.design/pro/" target="_blank">
-
-                                <svg className="icon-22 me-2" width="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M21.4274 2.5783C20.9274 2.0673 20.1874 1.8783 19.4974 2.0783L3.40742 6.7273C2.67942 6.9293 2.16342 7.5063 2.02442 8.2383C1.88242 8.9843 2.37842 9.9323 3.02642 10.3283L8.05742 13.4003C8.57342 13.7163 9.23942 13.6373 9.66642 13.2093L15.4274 7.4483C15.7174 7.1473 16.1974 7.1473 16.4874 7.4483C16.7774 7.7373 16.7774 8.2083 16.4874 8.5083L10.7164 14.2693C10.2884 14.6973 10.2084 15.3613 10.5234 15.8783L13.5974 20.9283C13.9574 21.5273 14.5774 21.8683 15.2574 21.8683C15.3374 21.8683 15.4274 21.8683 15.5074 21.8573C16.2874 21.7583 16.9074 21.2273 17.1374 20.4773L21.9074 4.5083C22.1174 3.8283 21.9274 3.0883 21.4274 2.5783Z" fill="currentColor"></path>
-                                    <path opacity="0.4" fillRule="evenodd" clipRule="evenodd" d="M3.01049 16.8079C2.81849 16.8079 2.62649 16.7349 2.48049 16.5879C2.18749 16.2949 2.18749 15.8209 2.48049 15.5279L3.84549 14.1619C4.13849 13.8699 4.61349 13.8699 4.90649 14.1619C5.19849 14.4549 5.19849 14.9299 4.90649 15.2229L3.54049 16.5879C3.39449 16.7349 3.20249 16.8079 3.01049 16.8079ZM6.77169 18.0003C6.57969 18.0003 6.38769 17.9273 6.24169 17.7803C5.94869 17.4873 5.94869 17.0133 6.24169 16.7203L7.60669 15.3543C7.89969 15.0623 8.37469 15.0623 8.66769 15.3543C8.95969 15.6473 8.95969 16.1223 8.66769 16.4153L7.30169 17.7803C7.15569 17.9273 6.96369 18.0003 6.77169 18.0003ZM7.02539 21.5683C7.17139 21.7153 7.36339 21.7883 7.55539 21.7883C7.74739 21.7883 7.93939 21.7153 8.08539 21.5683L9.45139 20.2033C9.74339 19.9103 9.74339 19.4353 9.45139 19.1423C9.15839 18.8503 8.68339 18.8503 8.39039 19.1423L7.02539 20.5083C6.73239 20.8013 6.73239 21.2753 7.02539 21.5683Z" fill="currentColor"></path>
-                                </svg>
-                                Go Pro
-                            </Nav.Link>
+                            
                             <Dropdown as="li" className="nav-item">
                                 <Dropdown.Toggle as={CustomToggle} variant="search-toggle nav-link">
                                     <img src={flag1} className="img-fluid rounded-circle" alt="user" style={{ height: "30px", minWidth: "30px", width: "30px", }} />
@@ -263,8 +263,17 @@ const Header = memo((props) => {
                                     <img src={avatars6} alt="User-Profile" className="theme-color-yellow-img img-fluid avatar avatar-50 avatar-rounded" />
                                     <img src={avatars4} alt="User-Profile" className="theme-color-pink-img img-fluid avatar avatar-50 avatar-rounded" />
                                     <div className="caption ms-3 d-none d-md-block ">
-                                        <h6 className="mb-0 caption-title">Austin Robertson</h6>
-                                        <p className="mb-0 caption-sub-title">Marketing Administrator</p>
+                                        {user ? (
+                                            <>
+                                                <h6 className="mb-0 caption-title">Welcome, {user.first_name} {user.last_name}</h6>
+                                                <p className="mb-0 caption-sub-title">Email: {user.email}</p>
+                                            </>
+                                        ) : (
+                                            <>
+                                                <h6 className="mb-0 caption-title">Welcome, Guest</h6>
+                                                <p className="mb-0 caption-sub-title">Please sign in</p>
+                                            </>
+                                        )}
                                     </div>
                                 </Dropdown.Toggle>
                                 <Dropdown.Menu className="dropdown-menu-end color-black" aria-labelledby="navbarDropdown">
