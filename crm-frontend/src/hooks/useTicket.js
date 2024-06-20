b79086cdd8ed1a44db3c622b858443ec48032c79
ILYAS CHAOUI <144786151+ichaoui56@users.noreply.github.com>
2024-06-20 12:16:54 +0100

    create a hook for tickets display

---
+++ crm-frontend/src/hooks/useTicket.js
@@ -0,0 +1,29 @@
+// src/hooks/useTickets.js
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useTickets = () => {
+    const [tickets, setTickets] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+            try {
+                const response = await axios.get('http://127.0.0.1:8000/api/tickets');
+                setTickets(response.data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching tickets:', error);
+                setError(error);
+                setLoading(false);
+            }
+        };
+
+        fetchTickets();
+    }, []);
+
+    return { tickets, loading, error };
+};
+
+export default useTickets;
